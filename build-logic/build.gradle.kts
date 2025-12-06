ee5ef655362b45f6b7c8223ca5c56c5959845e0f
Sven Zahrend <szsoftware@gmx.de>
2025-12-06 20:20:01 +0100

    Add `publishAllToMavenLocal` task for build-logic plugins

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -5,3 +5,10 @@ plugins {
     alias(libs.plugins.vanniktech.maven.publish) apply false
     id("gobley-gradle-build") apply false
 }
+
+tasks.register("publishAllToMavenLocal") {
+    group = "publishing"
+    description = "Publishes all build-logic plugins to Maven Local"
+
+    dependsOn(subprojects.mapNotNull { it.tasks.findByName("publishToMavenLocal") })
+}
